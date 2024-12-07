f8ca26980c57619290e391257f202a7c46648291
root <root@DESKTOP-UFFG1BQ>
2024-12-07 18:22:41 +0100

    nginx added to server static files

--- srcs/django/app/transcendence/views.py
+++ srcs/django/app/transcendence/views.py
@@ -1,6 +1,10 @@
+from collections import defaultdict
+from typing import DefaultDict, reveal_type
+from django.http.request import HttpRequest
 from django.shortcuts import render
-from django.http import HttpResponse, response
 
-def index(request):
-    username = "ketrevis"
+def index(request: HttpRequest):
+    username = ""
+    if "username" in request.GET:
+        username = request.GET.get('username')
     return render(request, "index.html", { "USERNAME": username })
