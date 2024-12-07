50f280e0df21cbed56302767ce6b063911683df8
root <root@DESKTOP-UFFG1BQ>
2024-12-07 16:56:37 +0100

    ssr wip

--- srcs/django/app/transcendence/views.py
+++ srcs/django/app/transcendence/views.py
@@ -2,7 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse, response
 
 def index(request):
-    return HttpResponse(b"Hello world")
-
-# def index(request):
-#     return render(request, "index.html")
+    username = "ketrevis"
+    return render(request, "index.html", { "USERNAME": username })
