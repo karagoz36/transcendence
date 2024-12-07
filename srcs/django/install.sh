c0703e39a11514afab5ed68b9111c1be09f41a61
root <root@DESKTOP-UFFG1BQ>
2024-12-07 16:50:29 +0100

    env template added

--- srcs/django/install.sh
+++ srcs/django/install.sh
@@ -2,8 +2,4 @@ if [ ! -e /app/manage.py ]; then
 	django-admin startproject transcendence app
 fi
 
-if [ !-e /app/views ]; then
-	python /app/manage.py startapp views
-fi
-
 python /app/manage.py runserver 0.0.0.0:8000
