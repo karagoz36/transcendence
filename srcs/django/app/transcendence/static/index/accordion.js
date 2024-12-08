91345824079a56a1ce96e64404d877d48ecf58e6
root <root@DESKTOP-UFFG1BQ>
2024-12-08 15:18:20 +0100

    accordion fix

--- srcs/django/app/transcendence/static/index/accordion.js
+++ srcs/django/app/transcendence/static/index/accordion.js
@@ -1,11 +1,14 @@
 // @ts-check
 /** @param {Event} e */
 function onAccordionClicked(e) {
+	const toFind = ".accordion-collapse.collapsing"
 	/** @type { NodeList } */
-	const accordionContent = document.querySelectorAll(".accordion-collapse.collapsing")
+	const accordionContent = document.querySelectorAll( toFind)
 
 	if (accordionContent.length == 1) {
-		const toChange = document.querySelector(".accordion-collapse.collapsing")
+		const toChange = document.querySelector(toFind)
+		if (!toChange)
+			throw new Error("failed to find toFind")
 		toChange?.classList.add("show")
 		toChange?.classList.add("collapse")
 		toChange?.classList.remove("collapsing")
@@ -14,8 +17,8 @@ function onAccordionClicked(e) {
 
 function keepOneAccordionOpened() {
 	/** @type { NodeListOf<HTMLButtonElement> } */
-	const buttons = document.querySelectorAll("button>.accordion-button")
-	buttons.forEach(button => button.onclick = onAccordionClicked)
+	const buttons = document.querySelectorAll("button.accordion-button")
+	buttons.forEach(button => button.addEventListener("click", onAccordionClicked))
 }
 
 keepOneAccordionOpened()
