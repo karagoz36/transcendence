f9eda6b52520d4d1d33779ed28b36e48229b850d
treviskevin77@gmail.com <treviskevin77@gmail.com>
2025-01-08 18:51:54 +0100

    responsive done

--- srcs/django/app/transcendence/static/pong/play.js
+++ srcs/django/app/transcendence/static/pong/play.js
@@ -15,18 +15,21 @@ camera.position.z = 10;
 const renderer = new THREE.WebGLRenderer();
 document.querySelector("#pong-container")?.appendChild(renderer.domElement);
 
-/** @param {number} position */
-function addPaddle(position) {
+/** 
+ * @param {number} position 
+ * @param {number} color
+ **/
+function addPaddle(position, color) {
     const geometry = new THREE.BoxGeometry(0.5, 3, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshBasicMaterial({ color });
     const paddle = new THREE.Mesh(geometry, material);
     paddle.position.x = position;
     scene.add(paddle);
     return paddle;
 }
 
-const paddle1 = addPaddle(10);
-const paddle2 = addPaddle(-10);
+const paddle1 = addPaddle(10, 0xff0000);
+const paddle2 = addPaddle(-10, 0x0000ff);
 
 function animate() {
     renderer.render(scene, camera);
@@ -37,7 +40,10 @@ renderer.setAnimationLoop(animate);
 function onWindowResize() {
     camera.aspect = ASPECT_RATIO;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    const height = window.innerWidth / ASPECT_RATIO
+    if (window.innerHeight > window.innerWidth) {
+    }
+    renderer.setSize(window.innerWidth, height, false);
 }
 
 window.addEventListener("resize", onWindowResize);
