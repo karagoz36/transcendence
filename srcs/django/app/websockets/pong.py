c43d8f2a7d907fb9d3b927d9a69367874c2d5e13
treviskevin77@gmail.com <treviskevin77@gmail.com>
2025-01-08 14:36:46 +0100

    changing position on direction received

--- srcs/django/app/websockets/pong.py
+++ srcs/django/app/websockets/pong.py
@@ -1,7 +1,6 @@
 import asyncio
 import redis
 from django.contrib.auth.models import User
-import json
 from utils.websocket import sendMessageWS
 
 redisClient = redis.StrictRedis(host="redis", port=6379, decode_responses=True)
@@ -16,11 +15,16 @@ class PongPlayer:
     def move(self):
         key = f"pong_direction:{self.user.id}"
         direction = redisClient.hgetall(key)
-        direction = direction.get("direction")
-        if direction is None:
-            print('no direction', flush=True)
-            return
+        direction: str|None = direction.get("direction")
+        match direction:
+            case "up":
+                self.position += 1
+            case "down":
+                self.position += 1
+            case None:
+                return        
         redisClient.delete(key)
+        print(self.position)
 
 async def gameLoop(user1: User, user2: User):
     p1 = PongPlayer(user1)
