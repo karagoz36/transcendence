ade7a667c89a2f2b27fb08e1fa7730e5e3a962e6
Taha Karagoz <tkaragoz@student.42.fr>
2025-01-27 12:11:36 +0100

    alermanager sending mails

--- srcs/monitoring/alertmanager/entrypoint.sh
+++ srcs/monitoring/alertmanager/entrypoint.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-# # Fail on any error
-# set -e
+CONFIG_FILE="/etc/alertmanager/alertmanager.yml"
+TEMP_FILE="/tmp/alertmanager.yml"
 
-# # Path to the Alertmanager configuration file
-# CONFIG_FILE="/etc/alertmanager/alertmanager.yml"
+if [ -z "$EMAIL_HOST_USER" ] || [ -z "$EMAIL_HOST_PASSWORD" ]; then
+  echo "EMAIL_HOST_USER and EMAIL_HOST_PASSWORD must be set."
+  exit 1
+fi
 
-# # Check if EMAIL_HOST_USER and EMAIL_HOST_PASSWORD environment variables are set
-# if [ -z "$EMAIL_HOST_USER" ] || [ -z "$EMAIL_HOST_PASSWORD" ]; then
-#   echo "Environment variables EMAIL_HOST_USER and EMAIL_HOST_PASSWORD must be set."
-#   exit 1
-# fi
+sed -e "s|\${EMAIL_HOST_USER}|$EMAIL_HOST_USER|g" \
+    -e "s|\${EMAIL_HOST_PASSWORD}|$EMAIL_HOST_PASSWORD|g" \
+    "$CONFIG_FILE" > "$TEMP_FILE"
 
-# # Replace placeholders in the configuration file with actual environment variable values
-# sed -i "s|\${EMAIL_HOST_USER}|$EMAIL_HOST_USER|g" "$CONFIG_FILE"
-# sed -i "s|\${EMAIL_HOST_PASSWORD}|$EMAIL_HOST_PASSWORD|g" "$CONFIG_FILE"
+if [ ! -s "$TEMP_FILE" ]; then
+  echo "Error: Empty config file after substitution"
+  exit 1
+fi
 
-# # Execute the original entrypoint command
-echo "HELLO"
-exec "$@"
+exec /bin/alertmanager \
+  --config.file="$TEMP_FILE" \
+  --cluster.advertise-address=:9093
+
